5a92a182389af2efac626e02fd1f1801c1bda493
Tobias Rupp <tobias_rupp@web.de>
2011-08-04 00:34:09 +0100

    batch file generator

--- lonelyplanet/app/controllers/conversion_controller.rb
+++ lonelyplanet/app/controllers/conversion_controller.rb
@@ -57,7 +57,7 @@ class ConversionController < ApplicationController
     parser = XML::Parser.file('public/input/destinations.xml')
     @destinations = parser.parse
     
-    #speed up XPath computation on static documents.
+    #speed up XPath computation on static documents
     @doc.order_elements! 
     @destinations.order_elements! 
     
@@ -76,6 +76,90 @@ class ConversionController < ApplicationController
     if @parent_atlas_node_id == nil
       @parent_atlas_node_id = parent_node_attributes.get_attribute('geo_id')    
     end
+  end
+  
+  def batch
+    #read and parse source files
+    parser = XML::Parser.file('public/input/taxonomy.xml')
+    @doc = parser.parse
+    parser = XML::Parser.file('public/input/destinations.xml')
+    @destinations = parser.parse
+    
+    #speed up XPath computation on static documents
+    @doc.order_elements! 
+    @destinations.order_elements! 
+    
+    #get all destination elements
+    @all_nodes = @doc.find("//node")
+    @all_nodes.each do |node| 
+      @node = node
+      
+      #get destination id
+      attributes = @node.attributes
+      @atlas_node_id = attributes.get_attribute('atlas_node_id')
+      if @atlas_node_id == nil
+        @atlas_node_id = attributes.get_attribute('geo_id')    
+      end
+    
+      #get destination text
+      @node.each_child do |element|
+    	  if element.name == 'node_name'  	
+    		  @node_name = element.content
+    		  break
+    	  end
+      end
+      if !@node_name 
+        @node_name = "Text not found"
+      end
+      
+      #get overview text of destination
+      @overview = @destinations.find_first("//destination[@atlas_id=#{@atlas_node_id.value}]/introductory/introduction/overview")
+      
+      #get parent destination
+      @parent_node = @node.parent
+      parent_node_attributes = @parent_node.attributes
+      @parent_atlas_node_id = parent_node_attributes.get_attribute('atlas_node_id')
+      if @parent_atlas_node_id == nil
+        @parent_atlas_node_id = parent_node_attributes.get_attribute('geo_id')    
+      end
+      
+      #get parent destination text
+      @parent_node.each_child do |element|
+    	  if element.name == 'node_name'  	
+    		  @parent_node_name = element.content
+    		  break
+    	  end
+      end
+      if !@parent_node_name 
+        @parent_node_name = "Text not found"
+      end
+    
+      #render output html and create new file at a specified path.
+      html_text = render_to_string  
+      file = File.new("public/output/#{@atlas_node_id.value}-#{@node_name}.html","w")
+      file.write html_text
+      file.close    
+     end
+     
+    render :text => 'Conversion completed.'
+    #render 'conversion/test'
+  end
+  
+  def testo
+    if !node_id = params[:id]
+      node_id = '355064'
+    end
+    parser = XML::Parser.file('public/input/taxonomy.xml')
+    @doc = parser.parse
+    parser = XML::Parser.file('public/input/destinations.xml')
+    @destinations = parser.parse
+    
+    #speed up XPath computation on static documents.
+    @doc.order_elements! 
+    @destinations.order_elements! 
+    
+    @nodes = @doc.find("//node")
+    render 'conversion/test'
   end
   
   def render_destination
@@ -118,6 +202,13 @@ class ConversionController < ApplicationController
     if @parent_atlas_node_id == nil
       @parent_atlas_node_id = parent_node_attributes.get_attribute('geo_id')    
     end
+    
+    html_text = render_to_string
+    
+    # Create new file at a specified path.
+    file = File.new("public/output/#{node_id}-#{@node_name}.html","w")
+    file.write html_text
+    file.close
   end
   
   def test
